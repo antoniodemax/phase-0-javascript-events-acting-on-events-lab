68a19a8f39161f787e7d54abc3867b3ea333d7e6
Anthony <anthony.onyango@student.moringaschool.com>
2024-07-07 18:48:04 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    const gameFieldWidth = 400; 
+    const dodgerWidth = 40; 
+  
+    if (left + dodgerWidth < gameFieldWidth) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    } else if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });
+
+  function moveDodgerLeft(){
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    if(left> 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+  }
+
